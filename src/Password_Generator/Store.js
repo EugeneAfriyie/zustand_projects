426c90a1c50852644c0d1ba85f9f1a372ea89297
Eugene Afriyie <groupeight00@gmail.com>
2025-05-09 23:46:48 +0000

    create the functionalities in usepaswordStore

--- src/Password_Generator/Store.js
+++ src/Password_Generator/Store.js
@@ -1,8 +1,5 @@
 import { create } from "zustand";
 
-
-
-
 const usePasswordStore = create((set) => ({
   password: "", 
   length: 12, 
@@ -11,6 +8,38 @@ const usePasswordStore = create((set) => ({
   includeNumbers: true, 
   includeSymbols: true, 
 
-  generatedPassword: "", // Initialize generated password state
-  setGeneratedPassword: (newGeneratedPassword) => set({ generatedPassword: newGeneratedPassword }), // Function to update generated password
+  generatedPassword: "",
+
+
+setLength:(length) => set({ length }),
+ toggleUppercase:() => (set((state) => ({ includeUppercase: !state.includeUppercase }))),
+ toggleNumbers:() => (set((state) => ({ includeNumbers: !state.includeNumbers }))),
+ toggleLowercase:() => (set((state) => ({ includeLowercase: !state.includeLowercase }))),
+ toggleSymbols:() => (set((state) => ({ includeSymbols: !state.includeSymbols }))),
+
+
+  generatePassword: ()  => set((state) => {
+    // const { length, includeUppercase, includeLowercase, includeNumbers, includeSymbols } = get();
+    const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+    let charSet = "";
+
+    if (state.includeUppercase) charSet += upperCaseChars;
+    if (state.includeLowercase) charSet += lowerCaseChars;
+    if (state.includeNumbers) charSet += numberChars;
+    if (state.includeSymbols) charSet += symbolChars;
+
+    let password = "";
+    for (let i = 0; i < state.length; i++) {
+      password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    }
+
+    return {generatedPassword: password};
+
+  }),
 }));
+
+
+export default usePasswordStore;
